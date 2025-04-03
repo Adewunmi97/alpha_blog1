e0cf7a40d059844653b0ab3270b6b490a4a058b3
adewunmi97 <shonibareadewunmicomfort@gmail.com>
2025-04-03 15:44:49 +0100

    add article listing feature

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#home"
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
   get 'about', to: 'pages#about'
 end
