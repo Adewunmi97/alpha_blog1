db037b5bcbaf8569081e3251c4d83a37f7af7f69
adewunmi97 <shonibareadewunmicomfort@gmail.com>
2025-04-07 14:33:04 +0100

    create new form with validations and messages

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#home"
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
   get 'about', to: 'pages#about'
 end
