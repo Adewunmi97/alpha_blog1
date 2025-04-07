8cbfdfbc56f60f4f1ec98f5e927e59fa25f71e60
adewunmi97 <shonibareadewunmicomfort@gmail.com>
2025-04-07 15:28:20 +0100

    add edit and update functionality

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#home"
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
   get 'about', to: 'pages#about'
 end
